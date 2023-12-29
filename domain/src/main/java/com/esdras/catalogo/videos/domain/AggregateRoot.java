46ddb615931daed36b76878104f7be4c3398b92a
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-12-28 23:04:31 -0300

    feat:ddd generic classes

--- domain/src/main/java/com/esdras/catalogo/videos/domain/AggregateRoot.java
+++ domain/src/main/java/com/esdras/catalogo/videos/domain/AggregateRoot.java
@@ -1,6 +1,8 @@
 package com.esdras.catalogo.videos.domain;
 
-
+/*
+o agregador raiz e um ponto de partida pra compor entidades e e objetos de valro
+ */
 public abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
 
         protected AggregateRoot(final ID id) {
