4ba6c958d6a14eced345322d5daf98d2e7b7c21b
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-12-31 20:16:37 -0300

    feat:tdd get use case finalizated

--- application/src/main/java/com/esdras/catalogo/videos/application/category/retrieve/get/DefaultGetCategoryByIdUseCase.java
+++ application/src/main/java/com/esdras/catalogo/videos/application/category/retrieve/get/DefaultGetCategoryByIdUseCase.java
@@ -1,8 +1,11 @@
 package com.esdras.catalogo.videos.application.category.retrieve.get;
 
 import com.esdras.catalogo.videos.domain.category.CategoryGateway;
-
+import com.esdras.catalogo.videos.domain.category.CategoryID;
+import com.esdras.catalogo.videos.domain.exceptions.DomainException;
+import com.esdras.catalogo.videos.domain.validation.Error;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 public class DefaultGetCategoryByIdUseCase extends GetCategoryByIdUseCase {
 
@@ -14,6 +17,17 @@ public class DefaultGetCategoryByIdUseCase extends GetCategoryByIdUseCase {
 
     @Override
     public CategoryOutput execute(final String anIn) {
-        return null;
+        final var anCategoryID = CategoryID.from(anIn);
+
+        return this.categoryGateway.findById(anCategoryID)
+                .map(CategoryOutput::from)
+                .orElseThrow(notFound(anCategoryID));
+    }
+
+
+    private Supplier<DomainException> notFound(final CategoryID anId) {
+        return () -> DomainException.with(
+                new Error("Category with ID %s was not found".formatted(anId.getValue()))
+        );
     }
 }
--- application/src/test/java/com/esdras/catalogo/videos/application/category/retrieve/get/GetCategoryByIdUseCaseTest.java
+++ application/src/test/java/com/esdras/catalogo/videos/application/category/retrieve/get/GetCategoryByIdUseCaseTest.java
@@ -4,6 +4,7 @@ import com.esdras.catalogo.videos.domain.category.Category;
 import com.esdras.catalogo.videos.domain.category.CategoryGateway;
 import com.esdras.catalogo.videos.domain.category.CategoryID;
 import com.esdras.catalogo.videos.domain.exceptions.DomainException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -35,6 +36,7 @@ public class GetCategoryByIdUseCaseTest {
     }
 
     @Test
+    @DisplayName("Dados um ID válido, quando chamar o método execute, então deve retornar uma categoria")
     public void givenAValidId_whenCallsGetCategory_shouldReturnCategory() {
         final var expectedName = "Filmes";
         final var expectedDescription = "A categoria mais assistida";
@@ -60,6 +62,7 @@ public class GetCategoryByIdUseCaseTest {
     }
 
     @Test
+    @DisplayName("Dados um ID inválido, quando chamar o método execute, então deve retornar uma exceção")
     public void givenAInvalidId_whenCallsGetCategory_shouldReturnNotFound() {
         final var expectedErrorMessage = "Category with ID 123 was not found";
         final var expectedId = CategoryID.from("123");
@@ -76,6 +79,7 @@ public class GetCategoryByIdUseCaseTest {
     }
 
     @Test
+    @DisplayName("Dados um ID válido, quando o gateway lança uma exceção, então deve retornar uma exceção")
     public void givenAValidId_whenGatewayThrowsException_shouldReturnException() {
         final var expectedErrorMessage = "Gateway error";
         final var expectedId = CategoryID.from("123");
