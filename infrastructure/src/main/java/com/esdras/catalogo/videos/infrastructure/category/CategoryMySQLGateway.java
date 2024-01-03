2236c9640d1a95961429254e9a154b1f0d129b34
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-01-02 21:14:52 -0300

    feat:tdd update finalizate

--- infrastructure/src/main/java/com/esdras/catalogo/videos/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/esdras/catalogo/videos/infrastructure/category/CategoryMySQLGateway.java
@@ -24,9 +24,10 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
 
     @Override
-    public Category create(Category aCategory) {
-        return this.repository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
+    public Category create(final Category aCategory) {
+        return save(aCategory);
     }
+
 
     @Override
     public void deleteById(CategoryID anId) {
@@ -39,12 +40,17 @@ public class CategoryMySQLGateway implements CategoryGateway {
     }
 
     @Override
-    public Category update(Category aCategory) {
-        return null;
+    public Category update(final Category aCategory) {
+        return save(aCategory);
     }
 
     @Override
     public Pagination<Category> findAll(CategorySearchQuery aQuery) {
         return null;
     }
+
+    private Category save(final Category aCategory) {
+        return this.repository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
+    }
+
 }
