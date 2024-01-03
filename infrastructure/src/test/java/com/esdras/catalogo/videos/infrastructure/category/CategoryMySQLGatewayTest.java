d7a5f086577da7e7479dd2f89d0bd417e111c53e
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-01-02 21:30:28 -0300

    feat:tdd find by Id Finalizated

--- infrastructure/src/test/java/com/esdras/catalogo/videos/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/esdras/catalogo/videos/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -150,4 +150,34 @@ public class CategoryMySQLGatewayTest {
         Assertions.assertEquals(0, categoryRepository.count());
     }
 
+    @Test
+    @DisplayName("Dado uma categoria válida, quando chamar o método findById, então deve retornar uma categoria")
+    public void givenAPrePersistedCategoryAndValidCategoryId_whenCallsFindById_shouldReturnCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais foda";
+        final var expectedIsActive = true;
+
+        final var aCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        categoryRepository.saveAndFlush(CategoryJpaEntity.from(aCategory));
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        final var actualCategory = categoryGateway.findById(aCategory.getId()).get();
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        Assertions.assertEquals(aCategory.getId(), actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+        Assertions.assertEquals(aCategory.getUpdatedAt(), actualCategory.getUpdatedAt());
+        Assertions.assertEquals(aCategory.getDeletedAt(), actualCategory.getDeletedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+
 }
