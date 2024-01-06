88d610baa269dcd5578690606180b8875ed71d64
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-01-06 12:00:19 -0300

    feat:adding docs open api

--- infrastructure/src/main/java/com/esdras/catalogo/videos/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/esdras/catalogo/videos/infrastructure/api/CategoryAPI.java
@@ -1,6 +1,10 @@
 package com.esdras.catalogo.videos.infrastructure.api;
 
 import com.esdras.catalogo.videos.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +14,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 //TANTO FAZ / OU SEM BARRA SPRING JA VE
 @RequestMapping(value = "categories")
+@Tag(name = "Categories")
 public interface CategoryAPI {
     //metodos que a api vai expor, e a documentação dela
     //RESPOSTA DE SUCESSO OU DE ERRO
+    //FICA MUITO MAIS CLEAN E FACIL DE ENTENDER
+    //SEPARAR ISSO DO CONTROLLER E FICA BEM MAIS CLEAN
 
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created successfully"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    })
+        //TEM MUITAS MAIS CONFIGURAÇOES DE DOCUMENTAÇÃO
     ResponseEntity<?> createCategory();
 
     //NO SPRING ELE JA FAZ O PROPRIO PARSE PRA GENTE PRA INT
     @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listed successfully"),
+            @ApiResponse(responseCode = "422", description = "A invalid parameter was received"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    })
     Pagination<?> listCategories(
             @RequestParam(name = "search", required = false, defaultValue = "") final String search,
             @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
