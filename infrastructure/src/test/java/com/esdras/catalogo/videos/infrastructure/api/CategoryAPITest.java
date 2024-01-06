4fbc55ce669e76a5eb01b27f700a53d7d2f04286
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-01-06 14:38:06 -0300

    feat:testing handler

--- infrastructure/src/test/java/com/esdras/catalogo/videos/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/esdras/catalogo/videos/infrastructure/api/CategoryAPITest.java
@@ -3,6 +3,8 @@ package com.esdras.catalogo.videos.infrastructure.api;
 import com.esdras.catalogo.videos.ControllerTest;
 import com.esdras.catalogo.videos.application.category.create.CreateCategoryOutput;
 import com.esdras.catalogo.videos.application.category.create.CreateCategoryUseCase;
+import com.esdras.catalogo.videos.domain.exceptions.DomainException;
+import com.esdras.catalogo.videos.domain.validation.Error;
 import com.esdras.catalogo.videos.domain.validation.handler.Notification;
 import com.esdras.catalogo.videos.infrastructure.category.models.CreateCategoryApiInput;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -118,6 +120,44 @@ public class CategoryAPITest {
 
 
         //VERIFICAR SE O USE CASE FOI CHAMADO UMA VEZ, E SE OS VALORES DOS ARGUMENTOS TEM O MESMO VALOR DO COMAMNDO CERTO
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+    }
+
+    @Test
+    @DisplayName("Dado um comando invalido, quando chama o caso de uso de criar categoria, então deve retornar uma exceção")
+    public void givenAInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() throws Exception {
+        // given
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedMessage = "'name' should not be null";
+
+        final var apiInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenThrow(DomainException.with(new Error(expectedMessage)));
+
+        // when
+        final var request = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(apiInput));
+
+        final var response = this.mvc.perform(request)
+                .andDo(print());
+
+        // then
+        response.andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Location", nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.message", equalTo(expectedMessage)))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)));
 
         verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
                 Objects.equals(expectedName, cmd.name())
