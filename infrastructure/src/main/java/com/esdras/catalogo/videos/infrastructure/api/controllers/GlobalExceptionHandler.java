bf6f5582877ea0242bac74ac65741485b382d122
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-01-06 14:53:30 -0300

    feat:handler global best way

---
+++ infrastructure/src/main/java/com/esdras/catalogo/videos/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.esdras.catalogo.videos.infrastructure.api.controllers;
+
+import com.esdras.catalogo.videos.domain.exceptions.DomainException;
+import com.esdras.catalogo.videos.domain.validation.Error;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+//PRA TRATAR AS EXCEPTIONS
+// UM HANDLER, E PRA CADA EXCEPTIONS VAMOS TRATAR BEM
+//ISSO AUQI E BOM QUE SO A PORRA, O CONTROLLER NÃO PRECISA FICAR COLOCANDO AS EXCEPTIONS LA
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //VAMOS DEFINIR TODOS OS HANDLERS DE EXCEPTIONS
+
+    //POR EXEMPLO PRA DOMAIN EXCEPTION VAMOS FAZER
+    //DIZENDO PRO SPRING QUE QUANDO DER UMA DOMAIN EXCEPTION, O QUE ELE TEM QUE FAZER
+    //PODE SER VARIOS EXCEPITONS PRA UM MESMO HANDLER
+    @ExceptionHandler(value = DomainException.class)
+    //NÃO VAMOS USAR, MAIS PODEMOS INJETAR EM OUTRO CASO PRA MANIPULAR O PAYLOAD COM ESSES
+    //HTTP SERVLET
+    public ResponseEntity<?> handleDomainException(
+            final DomainException ex,
+            final HttpServletRequest request,
+            final HttpServletResponse requestResponse
+    ) {
+        return ResponseEntity.unprocessableEntity().body(ApiError.from(ex));
+    }
+
+    record ApiError(String message, List<Error> errors) {
+        static ApiError from(final DomainException ex) {
+            return new ApiError(ex.getMessage(), ex.getErrors());
+        }
+    }
+}
